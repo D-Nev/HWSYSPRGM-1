33a72376ae3d449496d26145e0087349deb78b94
oop <opt@DESKTOP12>
2025-06-25 00:51:29 +0300

    w1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1;
+class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    static void Main(string[] args)
+    {
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            PlayGame();
+            playAgain = AskForReplay();
+        }
+        Console.WriteLine("Спасибо за игру!");
+    }
+
+    static void PlayGame()
+    {
+        Console.WriteLine("Загадайте число от 0 до 100.");
+        Console.WriteLine("Компьютер будет пытаться его угадать.");
+
+        int min = 0;
+        int max = 100;
+        int guess;
+        string userInput;
+
+        while (true)
+        {
+            guess = (min + max) / 2;
+            Console.WriteLine($"Компьютер предполагает, что ваше число: {guess}");
+            Console.WriteLine("Введите '+' если компьютер угадал, '-' если ваше число меньше, или '+' если ваше число больше.");
+            userInput = Console.ReadLine();
+
+            if (userInput == "+")
+            {
+                MessageBox(IntPtr.Zero, $"Компьютер угадал ваше число! Оно равно {guess}.", "Угаданное число", 0);
+                break;
+            }
+            else if (userInput == "-")
+            {
+                max = guess - 1;
+            }
+            else if (userInput == "+") 
+            {
+                min = guess + 1;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Повторите попытку.");
+            }
+        } 
+    }
+    static bool AskForReplay()
+    {
+        Console.WriteLine("Хотите сыграть еще раз? (да/нет)");
+        string response = Console.ReadLine();
+
+        return response.ToLower() == "да";
+    }
+}
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp2;
+
+class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Выберите приложение для запуска:");
+            Console.WriteLine("1. Блокнот");
+            Console.WriteLine("2. Калькулятор");
+            Console.WriteLine("3. Paint");
+            Console.WriteLine("4. Steam");
+            Console.WriteLine("5. Выход");
+            Console.Write("> ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Process.Start("notepad.exe");
+                    break;
+                case "2":
+                    Process.Start("calc.exe");
+                    break;
+                case "3":
+                    Process.Start("mspaint.exe");
+                    break;
+                case "4":
+                    Process.Start("\"C:\\Program Files (x86)\\Steam\\steam.exe\"");
+                    break;
+                case "5":
+                    return;
+                default:
+                    MessageBox(IntPtr.Zero, "!", "Ошибка", 0);
+                    break;
+            }
+        }
+    }
+}
